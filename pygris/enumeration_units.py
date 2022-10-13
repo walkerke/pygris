e2b1f7893191fe6b3ebcffcb6221f58230cc8d7a
Kyle Walker <kwalkerdata@gmail.com>
2022-10-13 08:26:34 -0500

    add support for places, pumas

--- pygris/enumeration_units.py
+++ pygris/enumeration_units.py
@@ -43,7 +43,40 @@ def counties(state = None, cb = False, resolution = '500k', year = None, cache =
     if year is None:
         print("Using the default year of 2021")
         year = 2021
+    """
+    Load a counties shapefile into Python as a GeoDataFrame
+
+    Parameters
+    ----------
+    state: The state name, state abbreviation, or two-digit FIPS code of the desired state. 
+           If None (the default), counties for the entire United States
+           will be downloaded.  
+
+    cb: If set to True, download a generalized (1:500k) cartographic boundary file.  
+        Defaults to False (the regular TIGER/Line file).
+
+    resolution: The resolution of the cartographic boundary file; only applies if 
+                the cb argument is set to True. The default is "500k"; options also
+                include "5m" (1:5 million) and "20m" (1:20 million)
     
+    year: The year of the TIGER/Line or cartographic boundary shapefile. If not specified,
+          defaults to 2021.
+
+    cache: If True, the function will download a Census shapefile to a cache directory 
+           on the user's computer for future access.  If False, the function will load
+           the shapefile directly from the Census website.  
+
+    Returns
+    ----------
+    geopandas.GeoDataFrame: A GeoDataFrame of counties.
+
+
+    Notes
+    ----------
+    See https://www2.census.gov/geo/pdfs/reference/GARM/Ch4GARM.pdf for more information. 
+
+
+    """
     if resolution not in ['500k', '5m', '20m']:
         raise ValueError("Invalid value for resolution. Valid values are '500k', '5m', and '20m'.")
     
@@ -353,3 +386,127 @@ def states(cb = True, resolution = "500k", year = None, cache = False):
             url = f"https://www2.census.gov/geo/tiger/TIGER{year}/STATE/tl_{year}_us_state.zip"
     
     return load_tiger(url, cache = cache)
+
+def pumas(state = None, cb = False, year = None, cache = False):
+    """
+    Load a public use microdata area (PUMA) shapefile into Python as a GeoDataFrame
+
+    Parameters
+    ----------
+    state: The state name, state abbreviation, or two-digit FIPS code of the desired state. 
+           If None (the default), PUMAs for the entire United States
+           will be downloaded.  
+
+    cb: If set to True, download a generalized (1:500k) cartographic boundary file.  
+        Defaults to False (the regular TIGER/Line file).
+  
+    year: The year of the TIGER/Line or cartographic boundary shapefile. If not specified,
+          defaults to 2021.
+
+    cache: If True, the function will download a Census shapefile to a cache directory 
+           on the user's computer for future access.  If False, the function will load
+           the shapefile directly from the Census website.  
+
+    Returns
+    ----------
+    geopandas.GeoDataFrame: A GeoDataFrame of PUMAs.
+
+
+    Notes
+    ----------
+    2020 PUMAs are available with `year = 2022` and later.  PUMAs are not available in the 2020 and 2021 CB files; 
+    use `year = 2019` or earlier to retrieve 2010 PUMAs.  
+    
+    See https://www.census.gov/programs-surveys/geography/guidance/geo-areas/pumas.html for more information. 
+
+
+    """
+    
+    if year is None:
+        year = 2021
+        print(f"Using the default year of {year}")
+    
+    if state is None:
+        if year == 2019 and cb:
+            state = "us"
+            print("Retrieving PUMAs for the entire United States")
+        else:
+            raise ValueError("A year must be specified for this year/dataset combination.")
+    else:
+        state = validate_state(state)
+    
+
+    if year > 2021:
+        suf = "20"
+    else:
+        suf = "10"
+    
+    if cb:
+        if year in [2020, 2021]:
+            raise ValueError("Cartographic boundary PUMAs are not yet available for years after 2019. Use the argument `year = 2019` instead to request your data.")    
+        else:
+            if year == 2013:
+                url = f"https://www2.census.gov/geo/tiger/GENZ{year}/cb_{year}_{state}_puma{suf}_500k.zip"
+            else:
+                url = f"https://www2.census.gov/geo/tiger/GENZ{year}/shp/cb_{year}_{state}_puma{suf}_500k.zip"
+    else:
+        url = f"https://www2.census.gov/geo/tiger/TIGER{year}/PUMA/tl_{year}_{state}_puma{suf}.zip"
+
+    pm = load_tiger(url, cache = cache)
+
+    return pm
+
+    
+def places(state = None, cb = False, year = None, cache = False):
+
+    """
+    Load a Census-designated places shapefile into Python as a GeoDataFrame
+
+    Parameters
+    ----------
+    state: The state name, state abbreviation, or two-digit FIPS code of the desired state. 
+           If None (the default), places for the entire United States
+           will be downloaded if available for that year / dataset combination.  
+
+    cb: If set to True, download a generalized (1:500k) cartographic boundary file.  
+        Defaults to False (the regular TIGER/Line file).
+  
+    year: The year of the TIGER/Line or cartographic boundary shapefile. If not specified,
+          defaults to 2021.
+
+    cache: If True, the function will download a Census shapefile to a cache directory 
+           on the user's computer for future access.  If False, the function will load
+           the shapefile directly from the Census website.  
+
+    Returns
+    ----------
+    geopandas.GeoDataFrame: A GeoDataFrame of Census-designated places.
+
+
+    Notes
+    ----------   
+    See https://www2.census.gov/geo/pdfs/reference/GARM/Ch9GARM.pdf for more information. 
+
+    """
+
+    if year is None:
+        year = 2021
+        print(f"Using the default year of {year}")
+    
+    if state is None:
+        if year == 2019 and cb:
+            state = "us"
+            print("Retrieving Census-designated places for the entire United States")
+        else:
+            raise ValueError("A year must be specified for this year/dataset combination.")
+    else:
+        state = validate_state(state)
+    
+    if cb:
+        url = f"https://www2.census.gov/geo/tiger/GENZ{year}/shp/cb_{year}_{state}_place_500k.zip"
+    else:
+        url = f"https://www2.census.gov/geo/tiger/TIGER{year}/PLACE/tl_{year}_{state}_place.zip"
+
+    return load_tiger(url, cache = cache)
+
+
